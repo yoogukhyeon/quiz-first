bd9d658c5a0a9958d2d3eaa7d00d4387d69d4a2f
yoogukhyeon <rnrgus5897@gmail.com>
2023-03-05 18:51:11 +0900

    code 수정

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,20 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { mainExcuteQuery } from '@/config/db';
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { mainExcuteQuery } from "@/config/db";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 interface Data {
-	message: string;
+  message: string;
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-	const method: string | undefined = req.method;
+  const method: string | undefined = req.method;
 
-	switch (method) {
-		case 'GET':
-			res.status(200).json({ message: 'hello' });
-			break;
-		default:
-			res.status(404).json({message: "api"});
-	}
+  switch (method) {
+    case "GET":
+      res.status(200).json({ message: "hello" });
+      break;
+    default:
+      res.status(404).json({ message: "api" });
+  }
 }
-``
