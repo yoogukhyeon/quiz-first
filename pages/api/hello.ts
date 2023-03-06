74263af99a26cfc845652ba7137161c83ddae845
yoogukhyeon <rnrgus5897@gmail.com>
2023-03-06 11:57:03 +0900

    structure changed

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,19 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { mainExcuteQuery } from "@/config/db";
-import type { NextApiRequest, NextApiResponse } from "next";
+import { mainExcuteQuery } from '@/config/db';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 interface Data {
-  message: string;
+	message: string;
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const method: string | undefined = req.method;
-
-  switch (method) {
-    case "GET":
-      res.status(200).json({ message: "hello" });
-      break;
-    default:
-      res.status(404).json({ message: "api" });
-  }
+	const method: string | undefined = req.method;
+	switch (method) {
+		case 'GET':
+			res.status(200).json({ message: 'hello' });
+			break;
+		default:
+			res.status(404).json({ message: 'api' });
+	}
 }
