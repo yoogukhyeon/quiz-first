d00defe17fe0b545331207ded6db66f6963bc011
yoogukhyeon <rnrgus5897@gmail.com>
2023-03-19 17:07:07 +0900

    build

--- config/db.ts
+++ config/db.ts
@@ -1,23 +1,29 @@
-import mysql from 'serverless-mysql';
+import mysql from "serverless-mysql";
 
+console.log("::::::::::::::::");
+console.log("::::::::::::::::");
+console.log("::::::::::::::::");
+console.log("process.env.DB_HOST ::", process.env.DB_HOST);
+console.log("::::::::::::::::");
+console.log("::::::::::::::::");
 const pool = mysql({
-	config: {
-		host: process.env.DB_HOST,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASSWORD,
-		port: 3306,
-		database: process.env.DB_DATABASE,
-	},
+  config: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: 3306,
+    database: process.env.DB_DATABASE,
+  },
 });
 
 export async function mainExcuteQuery({ query, values }: { query: string; values: any[] }) {
-	try {
-		const results = await pool.query(query, values);
-		await pool.end();
-		console.log('connected to db...');
-		return results;
-	} catch (error) {
-		console.log('not connected to db...');
-		return { error };
-	}
+  try {
+    const results = await pool.query(query, values);
+    await pool.end();
+    console.log("connected to db...");
+    return results;
+  } catch (error) {
+    console.log("not connected to db...");
+    return { error };
+  }
 }
