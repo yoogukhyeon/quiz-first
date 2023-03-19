3229d4b372b8dc73f7576253f1ef362a22c9b85e
yoogukhyeon <rnrgus5897@gmail.com>
2023-03-19 16:20:52 +0900

    quiz

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 
 export const quizTotal = async () => {
-  const result = await axios.get(`http://localhost:3000/api/quiz`, {
+  const result = await axios.get(`${process.env.NEXT_PUBLIC_DOMAIN}/api/quiz`, {
     auth: {
       username: process.env.NEXT_PUBLIC_BASIC_USERNAME!,
       password: process.env.NEXT_PUBLIC_BASIC_PASSWORD!,
