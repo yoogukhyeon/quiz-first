db5fd7d9067d3d6ecbbc2a2528df7faaabe2e664
yoogukhyeon <rnrgus5897@gmail.com>
2023-03-22 12:51:26 +0900

    build

--- next.config.js
+++ next.config.js
@@ -5,9 +5,6 @@ const nextConfig = {
 	//   rewrites() {
 	//     return [{ source: "/api/:path*", destination: "http://localhost:3000/:path*" }];
 	//   },
-	images: {
-		deviceSizes: [600, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-	},
 };
 
 module.exports = nextConfig;
