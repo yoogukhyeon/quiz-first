f738d7cdfdb73e9b078e2e2e809ecef027fbe917
yoogukhyeon <rnrgus5897@gmail.com>
2023-03-22 14:15:55 +0900

    build

--- pages/quiz.tsx
+++ pages/quiz.tsx
@@ -98,34 +98,34 @@ export default function Quiz({ id = 1 }: IProps) {
 	);
 }
 
-// export const getServerSideProps = async ({ query }: GetServerSidePropsContext) => {
-// 	const no: number = 1;
-// 	const { ref } = query;
+export const getServerSideProps = async ({ query }: GetServerSidePropsContext) => {
+	const no: number = 1;
+	const { ref } = query;
 
-// 	const id = await axios.post(
-// 		`${process.env.NEXT_PUBLIC_DOMAIN}/api/quiz/user`,
-// 		{
-// 			no,
-// 			referUrl: ref,
-// 		},
-// 		{
-// 			auth: {
-// 				username: process.env.NEXT_PUBLIC_BASIC_USERNAME!,
-// 				password: process.env.NEXT_PUBLIC_BASIC_PASSWORD!,
-// 			},
-// 		}
-// 	);
+	const id = await axios.post(
+		`${process.env.NEXT_PUBLIC_DOMAIN}/api/quiz/user`,
+		{
+			no,
+			referUrl: ref,
+		},
+		{
+			auth: {
+				username: process.env.NEXT_PUBLIC_BASIC_USERNAME!,
+				password: process.env.NEXT_PUBLIC_BASIC_PASSWORD!,
+			},
+		}
+	);
 
-// 	const quizList = await axios.get(`${process.env.NEXT_PUBLIC_DOMAIN}/api/quiz/type/${no}`, {
-// 		auth: {
-// 			username: process.env.NEXT_PUBLIC_BASIC_USERNAME!,
-// 			password: process.env.NEXT_PUBLIC_BASIC_PASSWORD!,
-// 		},
-// 	});
+	const quizList = await axios.get(`${process.env.NEXT_PUBLIC_DOMAIN}/api/quiz/type/${no}`, {
+		auth: {
+			username: process.env.NEXT_PUBLIC_BASIC_USERNAME!,
+			password: process.env.NEXT_PUBLIC_BASIC_PASSWORD!,
+		},
+	});
 
-// 	if (id?.data?.message === 'success' && quizList?.data?.message === 'success') {
-// 		return {
-// 			props: { id: id?.data?.data?.id, quizList: quizList?.data?.data },
-// 		};
-// 	}
-// };
+	if (id?.data?.message === 'success' && quizList?.data?.message === 'success') {
+		return {
+			props: { id: id?.data?.data?.id, quizList: quizList?.data?.data },
+		};
+	}
+};
