410947fda73b664c26ac4ebcad4925f9870a172c
yoogukhyeon <rnrgus5897@gmail.com>
2023-07-14 10:51:03 +0900

    fix: adfit 수정

--- components/adfit/kakaoAdfit.tsx
+++ components/adfit/kakaoAdfit.tsx
@@ -25,7 +25,7 @@ function KakaoAdFit() {
 		} else {
 			ins.setAttribute('data-ad-width', '728');
 			ins.setAttribute('data-ad-height', '90');
-			ins.setAttribute('data-ad-unit', 'DAN-RSJ7Jjur0ZeEEM8Q');
+			ins.setAttribute('data-ad-unit', 'DAN-0NAgORrvDnhOj6jX');
 		}
 
 		script.async = 'true';
