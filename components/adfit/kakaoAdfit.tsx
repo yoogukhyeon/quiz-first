f1697906acad787e11cbc2d992604eadf6695eee
yoogukhyeon <rnrgus5897@gmail.com>
2023-09-02 10:43:11 +0900

    fix: adfit 수정

--- components/adfit/kakaoAdfit.tsx
+++ components/adfit/kakaoAdfit.tsx
@@ -25,7 +25,7 @@ function KakaoAdFit() {
 		} else {
 			ins.setAttribute('data-ad-width', '728');
 			ins.setAttribute('data-ad-height', '90');
-			ins.setAttribute('data-ad-unit', 'DAN-gC75jj4WBOSJFEES');
+			ins.setAttribute('data-ad-unit', 'DAN-RSJ7Jjur0ZeEEM8Q');
 		}
 
 		script.async = 'true';
